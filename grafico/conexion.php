32a9bb83b654fe8d1e69ad5e571b69f03fa623c5
Yona <1302jona@gmail.com>
2018-11-25 07:14:12 -0600

    var

---
+++ grafico/conexion.php
@@ -0,0 +1,18 @@
+<?php
+//error_reporting(0);
+$con = mysqli_connect("localhost", "root", "")or die("Erorr con mysqli_Connect");
+mysqli_select_db($con,"compiladores")or die("Erorr seleccionar DB");
+
+/*
+if ($con->connect_errno)
+{
+	echo "Fallo al conectar a MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+	exit();
+}
+else
+{
+
+    echo 'conectado';
+
+}*/
+?>
--- grafico/grafico.php
+++ grafico/grafico.php
@@ -1,91 +1,82 @@
 <?php 	
 
+require('conexion.php');
 
 
+	$selectCaracter="SELECT * FROM constantes WHERE tipo='ent'";
 
-session_start();
-//$preanalisis=$_SESSION['preanalisis'];
-//echo "PREANAL ".$preanalisis;
-//include('analex.php');
-echo "<a href='../cuerpo.php'>Ver ALGORITMO</a><br>";
-$Tokens;//solo declaro la variable tokens
-$TipoTokens;
-$NumLinea;
-$final=$_SESSION['variable4']; //en la posicion 0 guarde el numero de vueltas
+	$selectEnteros = "SELECT * FROM constantes WHERE tipo ='c'";
+		
+		$result=$con->query($selectCaracter);
 
-
-$EnterosConst=array();
-$Arreglos=array();
-$ifs=array();
-
-/*$NumTokens=$final/3;
-echo "<b>numero de vueltas $final</b><br>";
-echo "<b>numero de tokens $NumTokens</b><br>";
-*/
-
-		//pasa la variables de session a los 3 arreglos
-		 for($i=0;$i<$final;$i++)
-		 {
-		 	echo $Tokens[$i]=$_SESSION['variable'][$i]; echo "  ";
-		 	
-		 	echo $TipoTokens[$i]=$_SESSION['variable2'][$i]; echo "  ";
-		 	
-		 	echo $NumLinea[$i]=$_SESSION['variable3'][$i]; echo "<br><br>";
-		 	
-		 }
-
-
-		 echo "--------------------------------------------------------------------<br>";
-
-
-
-
-		 for($i=0;$i<$final;$i++)
-		 {	
-		 	$Tokens[$i]=$_SESSION['variable'][$i]; 
-		 	$TipoTokens[$i]=$_SESSION['variable2'][$i]; 
-		 	$NumLinea[$i]=$_SESSION['variable3'][$i]; 
-
-
-		 		
-		 	if($TipoTokens[$i]=='id')
-		 	{
-		 		echo $Tokens[$i]; echo"="; echo $TipoTokens[$i];
-		 		echo "<br>";
-
-		 		
-
-		 		//$resultado = array_unique($Tokens);
-				//print_r($resultado);
-		 	}
-
-		 }
-$Tokens = array_values(array_unique($Tokens));
-
-
-		 	$final=count($Tokens);
-
-		 	 for($i=0;$i<$final;$i++)
-		 {
-		 	echo $Tokens[$i]; 
-		 	echo "<br>";
-		 }
-
-/*
-
-		 	$Tokens = array_values(array_unique($Tokens));
-
-
-		 	$final=count($Tokens);
-
-		 	 for($i=0;$i<$final;$i++)
-		 {
-		 	echo $Tokens[$i]; 
-		 	echo "<br>";
-		 }*/
-
+		$result2=$con->query($selectEnteros);
 
 
 
 
  ?>
+
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+
+ 	 <table>
+                        <thead>
+                            <tr class="centro">
+
+                              <td><b>lexema entero</b></td>
+                               
+                                
+                               <!-- <td colspan="2">Modificar Usuarios</td>-->
+                          
+                            </tr>
+                            <tbody>
+                                <?php while($row=$result->fetch_assoc()){ ?>
+                                    <tr>
+
+                                    <td>
+                                            <?php echo $row['lexema'];
+                                            	echo " = ";
+                                            	echo $row['valor'];
+                                            	?>
+                                       </td>
+			
+                                      
+                                    </tr>
+                                    <?php } ?>
+                            </tbody>
+                    </table>
+
+
+                     <table>
+                        <thead>
+                            <tr class="centro">
+
+                              <td><b>lexema caracter</b></td>
+                               
+                                
+                               <!-- <td colspan="2">Modificar Usuarios</td>-->
+                          
+                            </tr>
+                            <tbody>
+                                <?php while($row=$result2->fetch_assoc()){ ?>
+                                    <tr>
+
+                                    <td>
+                                            <?php echo $row['lexema'];
+                                   		         echo " = ";
+                                            	echo "'".$row['valor']."'";?>
+                                       </td>
+			
+                                      
+                                    </tr>
+                                    <?php } ?>
+                            </tbody>
+                    </table>
+ 
+ </body>
+ </html>
